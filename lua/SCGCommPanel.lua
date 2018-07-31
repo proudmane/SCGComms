f54db25e5070421aef3756344f4c7e3c96d70097
Feliaha <kylelawhorn@gmail.com>
2018-07-31 11:04:51 -0400

    route comm system through wow community api, bump addon version 8.0.1

--- lua/SCGCommPanel.lua
+++ lua/SCGCommPanel.lua
@@ -35,7 +35,7 @@ local w = {}
 function Me:SendComm(comm_string)
   if enabled == true then
     local emote = Me:SubValues(Main.db.char.patrolComms.emote)
-    SendChatMessage(comm_string, "CHANNEL", 7, GetChannelName("swradio"))
+    C_Club.SendMessage("345393461", "1", comm_string)
     SendChatMessage(emote, "EMOTE", nil, nil)
   else
     print("Comm String: "..comm_string)
