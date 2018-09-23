7ac8773a73064fcb012da9aa20beda961df8f4d0
Feliaha <kylelawhorn@gmail.com>
2018-09-22 20:06:32 -0400

    add print testing function and new testing module. move table tostring to testing module

--- lua/SCGConfigPanel.lua
+++ lua/SCGConfigPanel.lua
@@ -112,46 +112,6 @@ function Me:ConfigText(parent_key)
   w[parent_key]:AddChild(w[my_key])
 end
 
-function Me:PrintClubs()
-  w["config_text"]:SetText(table.tostring(C_Club.GetStreams("345393461")))
-end
-
-function table.val_to_str ( v )
-  if "string" == type( v ) then
-    v = string.gsub( v, "\n", "\\n" )
-    if string.match( string.gsub(v,"[^'\"]",""), '^"+$' ) then
-      return "'" .. v .. "'"
-    end
-    return '"' .. string.gsub(v,'"', '\\"' ) .. '"'
-  else
-    return "table" == type( v ) and table.tostring( v ) or
-      tostring( v )
-  end
-end
-
-function table.key_to_str ( k )
-  if "string" == type( k ) and string.match( k, "^[_%a][_%a%d]*$" ) then
-    return k
-  else
-    return "[" .. table.val_to_str( k ) .. "]"
-  end
-end
-
-function table.tostring( tbl )
-  local result, done = {}, {}
-  for k, v in ipairs( tbl ) do
-    table.insert( result, table.val_to_str( v ) )
-    done[ k ] = true
-  end
-  for k, v in pairs( tbl ) do
-    if not done[ k ] then
-      table.insert( result,
-        table.key_to_str( k ) .. "=" .. table.val_to_str( v ) )
-    end
-  end
-  return "{" .. table.concat( result, "," ) .. "}"
-end
-
 function Me:StartPatrolConfig(parent_key)
   local my_key = "start_patrol_config"
   w[my_key] = AceGUI:Create("EditBox")
