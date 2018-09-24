f55861ab563479ee3ba9f70c780c6dd27e162dda
Feliaha <34527602+proudmane@users.noreply.github.com>
2018-09-24 14:01:10 -0400

    more work on update panel

--- lua/UpdatePanel.lua
+++ lua/UpdatePanel.lua
@@ -22,11 +22,28 @@ end
 
 function Me:UpdatePanel()
   local my_key = "update_panel"
-  w[my_key] = AceGUI:Create("SimpleGroup")
+  w[my_key] = AceGUI:Create("Frame")
 
-  -- w[my_key]:SetTitle("Quick Update")
+  w[my_key]:SetTitle("Quick Update")
   w[my_key]:SetWidth(200)
   w[my_key]:SetHeight(200)
+  w[my_key]:SetLayout("List")
+
+  Me:UpdateButton(my_key)
 end
 
--- function
+function Me:UpdateButton(parent_key)
+  local my_key = "update_button"
+  w[my_key] = AceGUI:Create("Button")
+
+  w[my_key]:SetText("Update")
+
+  w[my_key]:SetCallback("OnClick",
+      function(button) Me:Test(button) end)
+
+  w[parent_key]:AddChild(w[my_key])
+end
+
+function Me:Test(button)
+  print(button)
+end
